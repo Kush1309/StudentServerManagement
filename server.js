ae7e345e2f4950b4187d8363204f06843df01167
Kush <kushagrasaxena0913@gamil.com>
2026-02-02 11:42:34 +0530

    Resolved merge conflicts

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json()); 
+app.use(express.json()); //middleware
 
 app.get("/", (req, res) => {
     res.send("Server is running");
