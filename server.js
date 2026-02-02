072d9226c148a34462f54cff920194af2129663c
Kush <kushagrasaxena0913@gamil.com>
2026-02-02 09:50:35 +0530

    Server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json()); 
+
+app.get("/", (req, res) => {
+    res.send("Server running");
+});
+
+app.post("/data", (req, res) => {
+    console.log(req.body); 
+    res.json({ success: true });
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
